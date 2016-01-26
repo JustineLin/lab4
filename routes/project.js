06f0bfbbd1fcec5fb5d257cf7aa654e959b75737
Justine <jcl084@ucsd.edu>
2016-01-26 00:16:27 +0000

    edit

--- routes/project.js
+++ routes/project.js
@@ -2,6 +2,6 @@ exports.viewProject = function(req, res) { 
   var name = req.params.name;
   console.log("The project name is: " + name);
   res.render('project',{
-  	'projectName': name
+  	'projectName': name   
   });
 };
